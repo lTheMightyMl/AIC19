f13e70eac37b79d8197959724a558dba80fd94d0
smsk <mohammadsadeghkeshavarzi@yahoo.com>
2019-02-17 21:49:48 +0330

    Only 4 dodges per action phase

--- AIC19-Client-Python-1.1.5/AI.py
+++ AIC19-Client-Python-1.1.5/AI.py
@@ -29,17 +29,17 @@ class AI:
 
     def move(self, world):
         print("move")
-        for a in range(4):
-            if world.map.objective_zone[a] == world.my_heroes[a].current_cell:
-                self.destinations_reached[a] = True
-        self.destination_reached = all(self.destinations_reached)
-        if not self.destination_reached:
-            for a in range(4):
-                if not self.destinations_reached[a]:
-                    destination = world.map.objective_zone[a]
-                    hero_to_move = world.my_heroes[a]
-                    start = hero_to_move.current_cell
-                    world.move_hero(hero=hero_to_move, direction=world.get_path_move_directions(start_cell=start, end_cell=destination)[0])
+        # for a in range(4):
+        #     if world.map.objective_zone[a] == world.my_heroes[a].current_cell:
+        #         self.destinations_reached[a] = True
+        # self.destination_reached = all(self.destinations_reached)
+        # if not self.destination_reached:
+        #     for a in range(4):
+        #         if not self.destinations_reached[a]:
+        #             destination = world.map.objective_zone[a]
+        #             hero_to_move = world.my_heroes[a]
+        #             start = hero_to_move.current_cell
+        #             world.move_hero(hero=hero_to_move, direction=world.get_path_move_directions(start_cell=start, end_cell=destination)[0])
 
     def action(self, world):
         print("action")
@@ -48,10 +48,7 @@ class AI:
                 self.destinations_reached[a] = True
         self.destination_reached = all(self.destinations_reached)
         if not self.destination_reached:
-            while self.destinations_reached[self.hero_to_act_number]:
-                self.hero_to_act_number += 1
-                self.hero_to_act_number %= 4
-            hero_to_act = world.my_heroes[self.hero_to_act_number]
-            world.cast_ability(hero=hero_to_act, ability=hero_to_act.dodge_abilities[0], cell=world.map.objective_zone[self.hero_to_act_number])
-            self.hero_to_act_number += 1
-            self.hero_to_act_number %= 4
+            for a in range(4):
+                if not self.destinations_reached[a]:
+                    hero_to_act = world.my_heroes[a]
+                    world.cast_ability(hero=hero_to_act, ability=hero_to_act.dodge_abilities[0], cell=world.map.objective_zone[a])
