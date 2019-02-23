ad04051412783b29f39031c067241012e765bf43
smsk <mohammadsadeghkeshavarzi@yahoo.com>
2019-02-23 21:15:39 +0330

    4 blasters seperately moving/dodging to ozone and bombing and attacking

--- Farthest/AI.py
+++ Farthest/AI.py
@@ -3,18 +3,14 @@ import Model
 
 class AI:
 
-    blaster_attack = []
     destination = []
-    destination_reached = False
-    destinations_reached = []
-    hero_to_act_number = 0
+    destination_reached = []
+    my_hero_number = 0
 
     def preprocess(self, world):
         print("preprocess")
-        self.blaster_attack.append(Model.AbilityName.BLASTER_BOMB)
-        self.blaster_attack.append(Model.AbilityName.BLASTER_ATTACK)
         for a in range(4):
-            self.destinations_reached.append(False)
+            self.destination_reached.append(False)
         mark = []
         for row in range(world.map.row_num):
             temp = []
@@ -22,7 +18,6 @@ class AI:
                 temp.append(False)
             mark.append(temp)
         for a in range(4):
-            print("a", a)
             for objective_cell in world.map.objective_zone:
                 if not mark[objective_cell.row][objective_cell.column]:
                     self.destination.append(objective_cell)
@@ -33,8 +28,6 @@ class AI:
                     if(len(world.get_path_move_directions(start_cell=source, end_cell=objective_cell)) > len(world.get_path_move_directions(start_cell=source, end_cell=self.destination[a]))):
                         self.destination[a] = objective_cell
             mark[self.destination[a].row][self.destination[a].column] = True
-            for row in mark:
-                print(row)
 
     def pick(self, world):
         print("pick")
@@ -54,48 +47,55 @@ class AI:
         print("move")
         for a in range(4):
             if self.destination[a] == world.my_heroes[a].current_cell:
-                self.destinations_reached[a] = True
+                self.destination_reached[a] = True
             else:
-                self.destinations_reached[a] = False
-        self.destination_reached = all(self.destinations_reached)
-        if not self.destination_reached:
-            for a in range(4):
-                if not self.destinations_reached[a]:
-                    destination = self.destination[a]
-                    hero_to_move = world.my_heroes[a]
-                    start = hero_to_move.current_cell
-                    world.move_hero(hero=hero_to_move, direction=world.get_path_move_directions(start_cell=start, end_cell=destination)[0])
+                self.destination_reached[a] = False
+        for a in range(4):
+            hero_to_move = world.my_heroes[a]
+            if hero_to_move.current_hp <= 0:
+                continue
+            if not self.destination_reached[a]:
+                destination = self.destination[a]
+                start = hero_to_move.current_cell
+                world.move_hero(hero=hero_to_move, direction=world.get_path_move_directions(start_cell=start, end_cell=destination)[0])
+
+    def ok(self, world, cell):
+        for hero in world.my_heroes:
+            if cell == hero.current_cell:
+                return False
+        return True
 
     def action(self, world):
         print("action")
         for a in range(4):
             if self.destination[a] == world.my_heroes[a].current_cell:
-                self.destinations_reached[a] = True
+                self.destination_reached[a] = True
             else:
-                self.destinations_reached[a] = False
-        self.destination_reached = all(self.destinations_reached)
-        if not self.destination_reached:
-            for a in range(4):
-                if not self.destinations_reached[a]:
-                    hero_to_act = world.my_heroes[a]
-                    world.cast_ability(hero=hero_to_act, ability=hero_to_act.dodge_abilities[0], cell=self.destination[a])
-        else:
-            if world.opp_heroes[0].current_cell.column == -1 and world.opp_heroes[0].current_cell.row == -1:
-                pass
+                self.destination_reached[a] = False
+        for a in range(4):
+            my_hero = world.my_heroes[a]
+            if my_hero.current_hp <= 0:
+                continue
+            destination = self.destination[a]
+            dodge = my_hero.dodge_abilities[0]
+            if not self.destination_reached[a] and dodge.is_ready and self.ok(world, world.get_impact_cell(ability=dodge, start_cell=my_hero.current_cell, target_cell=destination)):
+                world.cast_ability(hero=my_hero, ability=dodge, cell=destination)
             else:
-                nearest_opp = []
-                for a in range(4):
-                    nearest_opp.append(world.opp_heroes[0])
-                for a in range(4):
-                    my_hero_cell = world.my_heroes[a].current_cell
-                    for b in range(len(world.opp_heroes)):
-                        if world.opp_heroes[b].current_hp <= 0:
+                if world.opp_heroes[0].current_cell.column == -1 and world.opp_heroes[0].current_cell.row == -1:
+                    pass
+                else:
+                    nearest_opp = world.opp_heroes[0]
+                    my_hero_cell = my_hero.current_cell
+                    for opp_hero in world.opp_heroes:
+                        if opp_hero.current_hp <= 0:
                             continue
-                        if world.manhattan_distance(start_cell=my_hero_cell, end_cell=world.opp_heroes[b].current_cell) < world.manhattan_distance(start_cell=my_hero_cell, end_cell=nearest_opp[a].current_cell) :
-                            nearest_opp[a] = world.opp_heroes[b]
-                    if nearest_opp[a].current_hp > 0:
-                        for attack in self.blaster_attack:
-                            if attack.is_ready and world.manhattan_distance(start_cell=my_hero_cell, end_cell=nearest_opp[a].current_cell) <= attack.range:
-                                world.cast_ability(hero=world.my_heroes[a], ability=attack, cell=nearest_opp[a].current_cell)
-                                break
+                        if world.manhattan_distance(start_cell=my_hero_cell, end_cell=opp_hero.current_cell) < world.manhattan_distance(start_cell=my_hero_cell, end_cell=nearest_opp.current_cell):
+                            nearest_opp = opp_hero
+                    if nearest_opp.current_hp > 0:
+                        offensive_abilities = my_hero.offensive_abilities
+                        for b in range(len(offensive_abilities) - 1, -1, -1):
+                            attack = offensive_abilities[b]
+                            if attack.range < world.manhattan_distance(start_cell=my_hero_cell, end_cell=nearest_opp.current_cell) or not attack.is_ready:
+                                continue
+                            world.cast_ability(hero=my_hero, ability=attack, cell=nearest_opp.current_cell)
 
